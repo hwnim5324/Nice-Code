a4aff0301389a7c11bd2420456353c79b48a567f
hwnim5324 <hwnim5324@naver.com>
2022-09-30 15:56:08 +0900

    ui in progress

--- pages/accounts/signin.tsx
+++ pages/accounts/signin.tsx
@@ -1,5 +1,7 @@
 import type { NextPage } from 'next';
 import styled from '@emotion/styled';
+import Link from 'next/link';
+import { useState } from 'react';
 
 const StyledOutterDiv = styled.div`
     display: flex;
@@ -17,38 +19,92 @@ const StyledInnerDiv = styled.div`
 `;
 
 const StyledInput = styled.input`
-    width: 360px;
+    font-family: 'Noto Sans Kr';
+    font-size: 16px;
+
+    width: 400px;
     height: 52px;
 
     border: 0;
     border-bottom: 1px solid #ebebeb;
 
     margin: 0 0 30px 0;
+    padding: 0 0 0 14px;
 `;
 
 const StyledButton = styled.button`
-    width: 360px;
+    font-family: 'Noto Sans Kr';
+    font-size: 16px;
+
+    width: 400px;
     height: 52px;
 
     margin: 30px 0 0 0;
-    background-color: #ebebeb;
     border: 0;
+
+    &:enabled{
+        background-color: #333;
+        &:hover{
+            background-color: #ebebeb;
+        }
+    }
+    &:disabled{
+        background-color: #ebebeb;
+    }
 `;
 
+const StyledUl = styled.ul`
+    width: 400px;
+    display: block;
+    float: left;
+    font-size: 14px;
+
+    padding: 0 0 0 0;
+    margin : 15px 0 0 0;
+
+    li{
+        float: left;
+        list-style: none;
+
+        a{
+            text-decoration: none;
+            color: black;
+        }
+
+        &:first-child::after{
+            content: '|';
+            margin: 0 0 0 10px;
+        }
+
+        &:nth-child(2){
+            margin: 0 0 0 10px; 
+        }
+
+        &:last-child{
+            float: right;
+        }
+    }
+`
+
 const SignIn: NextPage = () => {
+    const [id, setId] = useState('');
+    const [pw, setPw] = useState('');
+
+    function submitSignin(id: string, pw: string){
+        // do axios
+    }
+
     return (
         <StyledOutterDiv>
             <StyledInnerDiv>
-                <StyledInput type='text' placeholder='아이디'/>
-                <StyledInput type='password' placeholder='비밀번호'/>
-                <StyledButton>로그인</StyledButton>
-                <div>
-                    <ul>
-                        <li>아이디 찾기</li>
-                        <li>비밀번호 찾기</li>
-                        <li>회원가입</li>
-                    </ul>
-                </div>
+                <StyledInput type='text' placeholder='아이디' onChange={(e)=>setId(e.target.value)}/>
+                <StyledInput type='password' placeholder='비밀번호' onChange={(e)=>setPw(e.target.value)}/>
+                <StyledButton disabled={id!==''&&pw!==''?false:true} onClick={()=>{submitSignin(id,pw);}}>로그인</StyledButton>
+                <StyledUl>
+                    <li><Link href='/accounts/findId'><a>아이디 찾기</a></Link></li>
+                    <li><Link href='/accounts/findPw'><a>비밀번호 찾기</a></Link></li>
+                    <li><Link href='/accounts/signup'><a>회원가입</a></Link></li>
+                </StyledUl>
             </StyledInnerDiv>
         </StyledOutterDiv>
     )
