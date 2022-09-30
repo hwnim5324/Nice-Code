d523f8b69a63c238c973f5b80e0a3bab838011d5
hwnim5324 <hwnim5324@naver.com>
2022-09-30 14:46:36 +0900

    add global style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,19 @@
 import type { AppProps } from 'next/app'
 import Layout from '../components/layout';
+import { css, Global } from '@emotion/react';
 
 const App = ( { Component, pageProps }: AppProps ) => {
     return (
         <>
+            <Global styles={{
+                '@font-face': {
+                    fontFamily: 'Noto Sans KR',
+                    src: 'url(https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap)'
+                },
+                body: {
+                    fontFamily: 'Noto Sans KR'
+                }
+            }}/>
             <Layout>
                 <Component {...pageProps} />
             </Layout>
