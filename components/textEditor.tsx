bca8f974ab34bb6bd77a92b6f67b72871ff9beb8
hwnim5324 <hwnim5324@naver.com>
2022-10-06 19:42:34 +0900

    post page in progress.

--- components/textEditor.tsx
+++ components/textEditor.tsx
@@ -2,6 +2,41 @@ import { useRef } from 'react';
 import { Editor } from '@toast-ui/react-editor';
 import '@toast-ui/editor/dist/toastui-editor.css';
 
+import { SaveButton } from '../styles/boards.styles';
+
+const Title = () => {
+    return (
+        <>
+            <input type='text' placeholder='제목을 입력해주세요.' />
+        </>
+    );
+}
+
+const Category = () => {
+    return (
+        <>
+            <h2>카테고리를 설정해주세요.</h2>
+            <ul>
+                <li><input type='radio' name='category' id='a'/><label for='a'>코드리뷰</label></li>
+                <li><input type='radio' name='category' id='b'/><label for='b'>질문</label></li>
+                <li><input type='radio' name='category' id='c'/><label for='c'>정보 게시판</label></li>
+                <li><input type='radio' name='category' id='d'/><label for='d'>자유 게시판</label></li>
+            </ul>
+        </>
+    );
+}
+
+const Tag = () => {
+    return (
+        <>
+            <h2>태그를 설정해주세요.</h2>
+            <select>
+                <option value=''></option>
+            </select>
+        </>
+    );
+}
+
 const TextEditor = () => {
     const editorRef = useRef();
 
@@ -14,6 +49,9 @@ const TextEditor = () => {
     return (
         <>
             <h1>새 포스트 작성</h1>
+            <Title />
+            <Category />
+            <Tag />
             <Editor
                     placeholder="내용을 입력해주세요."
                     previewStyle="vertical"
@@ -24,7 +62,10 @@ const TextEditor = () => {
                     initialValue=""
                     ref={editorRef}
             ></Editor>
-            <button onClick={savePost}>저장</button>
+            <div>
+                <SaveButton >저장하기</SaveButton>
+                <SaveButton onClick={savePost}>게시하기</SaveButton>
+            </div>
         </>
     );
 }
