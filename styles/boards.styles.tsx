60fc6bd1cf4f242906075a6bd60b3843b565a232
hwnim5324 <hwnim5324@naver.com>
2022-10-06 19:42:07 +0900

    style in progress.

---
+++ styles/boards.styles.tsx
@@ -0,0 +1,16 @@
+import styled from "@emotion/styled";
+
+const SaveButton = styled.button`
+    margin: 40px 0 0 0;
+    width: 240px;
+    height: 48px;
+
+    border: none;
+    background-color: black;
+    color: white;
+
+    font-size: 16px;
+    font-weight: 600;
+`;
+
+export { SaveButton };
