a4f131d3dfc4ab83c8c84bad6c69aa3ecbdf706c
hwnim5324 <hwnim5324@naver.com>
2022-10-07 22:18:44 +0900

    post list in progress.

--- pages/boards/free/index.tsx
+++ pages/boards/free/index.tsx
@@ -1,9 +1,21 @@
 import type { NextPage } from 'next';
+import Link from 'next/link';
 
 const FreeBoard: NextPage = () => {
     return (
         <div>
-            board
+            <div>
+                <ul>
+                    <li><Link href=''><a></a></Link></li>
+                </ul>
+            </div>
+            <div>
+                <button><</button>
+                <ul>
+                    <li><Link href=''><a></a></Link></li>
+                </ul>
+                <button>></button>
+            </div>
         </div>
     )
 }
