9913666cfee30a63f086421a4acd5a158713e5ec
hwnim5324 <hwnim5324@naver.com>
2022-10-11 22:20:48 +0900

    postlist styles in progress.

--- pages/boards/free/index.tsx
+++ pages/boards/free/index.tsx
@@ -1,21 +1,25 @@
 import type { NextPage } from 'next';
 import Link from 'next/link';
 
+import { PageList, PageNumList, PostList, Posts, Post } from '../../../styles/boards.styles';
+
 const FreeBoard: NextPage = () => {
     return (
         <div>
-            <div>
-                <ul>
-                    <li><Link href=''><a></a></Link></li>
-                </ul>
-            </div>
-            <div>
-                <button><</button>
-                <ul>
-                    <li><Link href=''><a></a></Link></li>
-                </ul>
-                <button>></button>
-            </div>
+            <PostList>
+                <Posts>
+                    <Post><Link href=''><a>1번 게시물</a></Link></Post>
+                    <Post><Link href=''><a>2번 게시물</a></Link></Post>
+                </Posts>
+            </PostList>
+            <PageList>
+                <button>&#60;</button>
+                <PageNumList>
+                    <li><Link href=''><a>1</a></Link></li>
+                    <li><Link href=''><a>2</a></Link></li>
+                </PageNumList>
+                <button>&#62;</button>
+            </PageList>
         </div>
     )
 }
--- styles/boards.styles.tsx
+++ styles/boards.styles.tsx
@@ -13,4 +13,47 @@ const SaveButton = styled.button`
     font-weight: 600;
 `;
 
-export { SaveButton };
+const PageList = styled.div`
+    display: flex;
+    justify-content: center;
+
+`
+
+const PageNumList = styled.ul`
+    list-style: none;
+    display: flex;
+    padding: 0 20px 0 20px;
+
+    li{
+        margin: 0 6px 0 6px;
+    }
+
+    a{
+        color: black;
+        text-decoration: none;
+    }
+`
+
+const PostList = styled.div`
+    
+`
+
+const Posts = styled.ul`
+    list-style: none;
+    padding: 0 0 0 0;
+`;
+
+const Post = styled.li`
+    display: flex;
+    width: 100%;
+    height: 52px;
+    align-items: center;
+
+    a{
+        color: black;
+        text-decoration: none;
+    }
+`;
+
+
+export { SaveButton, PageList, PageNumList, PostList, Posts, Post };
