40437e03c48b9d4de20b6be4f8ec597985d78e2c
hwnim5324 <hwnim5324@naver.com>
2022-10-14 21:31:31 +0900

    another board in progress.

--- pages/boards/info/index.tsx
+++ pages/boards/info/index.tsx
@@ -1,9 +1,25 @@
 import type { NextPage } from 'next';
+import Link from 'next/link';
+
+import { PageList, PageNumList, PostList, Posts, Post } from '../../../styles/boards.styles';
 
 const InfoBoard: NextPage = () => {
     return (
         <div>
-            board
+            <PostList>
+                <Posts>
+                    <Post><Link href=''><a>1번 게시물</a></Link></Post>
+                    <Post><Link href=''><a>2번 게시물</a></Link></Post>
+                </Posts>
+            </PostList>
+            <PageList>
+                <button>&#60;</button>
+                <PageNumList>
+                    <li><Link href=''><a>1</a></Link></li>
+                    <li><Link href=''><a>2</a></Link></li>
+                </PageNumList>
+                <button>&#62;</button>
+            </PageList>
         </div>
     )
 }
